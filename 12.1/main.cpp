60b7dd6ebeb2c64ad359d417705756bd66a91114
KonTN <tresh9090568868@yandex.ru>
2021-06-12 04:23:12 +0300

    vec first

---
+++ 12.1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    // get vector size
+    int vec_cop;
+    std::cout << "Enter vector capacity: ";
+    std::cin >> vec_cop;
+    if (vec_cop<=0) {std::cerr << "vector capacity must be > 0"; return -1;}
+
+    // get vector values
+    std::vector<int> num_vec;
+    int buff;
+    for (int i = 0; i<vec_cop;i++){
+        std::cout << "  enter vector value "<< '(' << i+1 <<" of " << vec_cop <<"): ";
+        std::cin >> buff;
+        num_vec.push_back(buff);
+    }
+
+    // erasing value
+    int erase_val;
+    std::cout << "Enter erase value: ";
+    std::cin >> erase_val;
+    std::vector<int> copy_vec;
+    for (int i : num_vec){
+        if (i!=erase_val) copy_vec.push_back(i);
+    }
+    num_vec = copy_vec;
+
+
+    // cout vector
+    for (int i : num_vec){
+        std::cout << i << ' ';
+    }
+
+
+    return 0;
+}
