26611a47e3dc43243fd7ed5cec66f3a209fdd056
константин тукмачёв <tresh9090568868@yandex.ru>
2021-11-02 05:34:06 +0300

    posible two

--- 14.1/main.cpp
+++ 14.1/main.cpp
@@ -1,30 +1,49 @@
 #include <iostream>
 # include <vector>
-
-short coord[2];
-
-bool positivity_test(std::vector<int> mass){
-    for (int i : mass) if (i>0) return true;
-    return false;
+std::vector<int> mass = {2,1,3,4,1,2,1,5,4};
+int coord[2];
+int  summ(int start,int end){
+    int resoult =  0;
+    for (int i = start;i<end;i++){
+        resoult += mass[i];
+    }
+    return resoult;
 }
-
 
 int main(){
 // исходный массив 
-std::vector<int> mass = {-2,1,-3,4,-1,2,1,-5,4};
-std::vector<int> massN = {-2,-1,-3,-4,-1,-2,-1,-5,-4};
 
-// should start and end with positive number
-// cant start or end with negativiti sequence (like: 1 -1 -1 or 5 -4 -7)
-
-
-// check that positivity number exist 
-        // if not the bigger one would be the clothest to 0 number;
     
+    // fiend the biggets number
+    int big = -2147483648;
+    for (int i: mass) if (i>big) big = i;
 
-// find first positivity nimber
+    // find all posible starts
+    std::vector<int> posible_starts = {0};
+    for (int i=0;i<mass.size()-1;i++){
+        if ((mass[i]<=0)&&(mass[i+1]>0)) posible_starts.push_back(i+1);
+    }
 
-    std::cout << positivity_test(mass) << ' ' << positivity_test(massN);
-    system("pause");
+    // find all posible ends
+    std::vector<int> posible_ends = {(int)mass.size()-1};
+    for (int i=0;i<mass.size()-1;i++){
+        if ((mass[i]>0)&&(mass[i+1]<=0)) posible_ends.push_back(i);
+    }
+    
+    for (int i : posible_starts){
+        for (int j : posible_ends){
+            int bigS = summ(i,j); 
+            if (bigS>big){
+                big = bigS;
+                coord[0] = i;
+                coord[1] = j;
+            }
+        }
+    }
+
+    
+    for (int i=0;i<mass.size();i++) if (mass[i]==big) {coord[0] = i; coord[1] = i;}
+    std::cout << "the biggest sequens starts from : " << coord[0] << " and ends with : " << coord[1] << std::endl;
+    system("pause");   
     return 0;
 }
