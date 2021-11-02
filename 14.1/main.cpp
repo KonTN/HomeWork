dcd1c22c9b605a65efa3752b52a774fee3d63f04
константин тукмачёв <tresh9090568868@yandex.ru>
2021-11-02 05:54:44 +0300

    first done

--- 14.1/main.cpp
+++ 14.1/main.cpp
@@ -2,7 +2,7 @@
 # include <vector>
 std::vector<int> mass = {-2,1,-3,4,-1,2,1,-5,4};
 int coord[2];
-int  summ(int start,int end){
+int  summ(std::vector<int> mass, int start,int end){
     int resoult =  0;
     for (int i = start;i<end;i++){
         resoult += mass[i];
@@ -29,10 +29,22 @@ int main(){
     for (int i=0;i<mass.size()-1;i++){
         if ((mass[i]>0)&&(mass[i+1]<=0)) posible_ends.push_back(i);
     }
+
+    // the biggest sequence can`t start with negative sequence
+    posible_starts.reserve(posible_starts.size());
+    for (int i=posible_starts.size();i>1;i--){
+        if (summ(posible_starts,i,i--)<0) posible_starts.pop_back();
+    }
+    posible_starts.reserve(posible_starts.size());
     
+    // the biggest sequence can`t end with negative sequence
+    for (int i=posible_ends.size();i>1;i--){
+        if (summ(posible_ends,i,i--)<0) posible_ends.pop_back();
+    }
+
     for (int i : posible_starts){
         for (int j : posible_ends){
-            int bigS = summ(i,j); 
+            int bigS = summ(mass,i,j); 
             if (bigS>big){
                 big = bigS;
                 coord[0] = i;
