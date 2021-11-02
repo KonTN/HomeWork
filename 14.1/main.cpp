1ee891de6e9ac1c0caf39eead7f1849da7b9d996
константин тукмачёв <tresh9090568868@yandex.ru>
2021-11-02 05:05:32 +0300

    posible one

--- 14.1/main.cpp
+++ 14.1/main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 # include <vector>
 
-std::vector<int> mass = {-2,1,-3,4,-1,2,1,-5,4};
 short coord[2];
 
+bool positivity_test(std::vector<int> mass){
+    for (int i : mass) if (i>0) return true;
+    return false;
+}
+
+
 int main(){
-    int summ = 0,buff = 0;
-    for (int i=0;i<mass.size();i++){
-        for (int j=i;j<mass.size();j++){
-          for (int k=i;k<j;k++) buff += mass[k];
-          if (buff>summ) {
-              coord[0] = i; coord[1] = j-1;
-              summ = buff;
-          }
-          buff = 0;
-        }
-    }
+// исходный массив 
+std::vector<int> mass = {-2,1,-3,4,-1,2,1,-5,4};
+std::vector<int> massN = {-2,-1,-3,-4,-1,-2,-1,-5,-4};
+
+// should start and end with positive number
+// cant start or end with negativiti sequence (like: 1 -1 -1 or 5 -4 -7)
 
 
-    std::cout << coord[0] << ' ' << coord[1] << std::endl;
+// check that positivity number exist 
+        // if not the bigger one would be the clothest to 0 number;
+    
+
+// find first positivity nimber
+
+    std::cout << positivity_test(mass) << ' ' << positivity_test(massN);
     system("pause");
-
-
-
-
-
-
     return 0;
 }
