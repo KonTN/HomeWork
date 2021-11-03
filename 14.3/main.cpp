42fc0fbc24fdb18b2712957a8e545d75612d9fb0
константин тукмачёв <tresh9090568868@yandex.ru>
2021-11-03 04:37:57 +0300

    third done

--- 14.3/main.cpp
+++ 14.3/main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
-#include <set>
+#include <vector>
+
+
+void sort_mass(std::vector<int> &mass){
+    for (int i=0;i<mass.size();i++){
+        for (int j=mass.size()-1;j>i;j--){
+            if (mass[j]<mass[j-1]){
+                int buff = mass[j];
+                mass[j] = mass[j-1];
+                mass[j-1] = buff;
+            }
+        }
+    }
+}
 
 
 int main(){
-    std::set<int> num;
+    std::vector<int> num;
     int buff;
     do{
         do{
         std::cin >> buff;
-        num.insert(buff);
-        } while ((buff!=-1)||(buff!=-2));
-        // dump way to do that -----------
-        int i=0;
-        int value;
-        for(int i: num){
-            i++;
-            if (i==5) {value = i; break;}
-        }
-        //--------------------------------
+        num.push_back(buff);
+        } while (!((buff==-1)||(buff==-2)));
+        num.pop_back(); // delete -1 or -2 from mass;
+        sort_mass(num);
         system("cls");
-        std::cout<< value << std::endl;
-        system("pause");
+        std::cout << std::endl;
+        for (int i: num) std::cout << i << ' ';
+        std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout<< num[4] << std::endl;
     }while (buff!=-2);
     return 0;
 }
