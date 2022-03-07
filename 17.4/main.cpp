b086a481707cf1ba3670ce66fe561b615b47e726
KonTN <tresh9090568868@yandex.ru>
2022-03-07 23:19:53 +0000

    fix 4

--- 17.4/main.cpp
+++ 17.4/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 void f(long long n, int& ans){
-   
-    if (n==0) return ;
+    
     if (n%2==0) ans+=1; 
+    if (n<10) return ;
     f(n/10,ans);
 }
 
@@ -12,7 +12,7 @@ void f(long long n, int& ans){
 int main(int argc, char** argv){
 
     int ans = 0;
-    f(9223372036854775806, ans);
+    f(0, ans);
     std::cout << ans << std::endl;
 
     return 0;
