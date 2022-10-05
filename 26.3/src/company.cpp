957049e207fd0bdf2f397ab6f9b6a93cf977e0fb
KonTN <tresh9090568868@yandex.ru>
2022-10-05 09:09:39 +0300

    company var

--- 26.3/src/company.cpp
+++ 26.3/src/company.cpp
@@ -19,13 +19,13 @@ int company::Worker::set_job(char job)
 {
     if ((job != 'A')&&(job != 'B')&&(job != 'C'))
     {
-        std::cout << myName << ": received wrong job ( " << job << " )\n";
+        std::cout << "\t" << myName << ": received wrong job ( " << job << " )\n";
         return 1;
     }
     else 
     {
         myJob = job;
-        std::cout << myName << " : received " << myJob << " job.\n";
+        std::cout << "\t" << myName << " : received " << myJob << " job.\n";
         return 0;
     }
 
@@ -37,7 +37,13 @@ char company::Worker::get_job()
 };
 
 
-
+company::Manager::Manager()
+{
+    myName = "UNKNOWN";
+    myId = -1;
+    myTeam.clear();
+    freeTeamatesCouter = -1;
+};
 
 company::Manager::Manager(std::string name, int id , std::vector<std::string> teamNames)
 {
@@ -47,11 +53,92 @@ company::Manager::Manager(std::string name, int id , std::vector<std::string> te
     {
         myTeam.push_back(Worker(wName));
     }
-    myJob = -1;
+    freeTeamatesCouter = myTeam.size();
 };
 
-int company::Manager::set_job(int job)
+bool company::Manager::has_free_worker()
 {
-    
+    for (Worker w : myTeam)
+    {
+        if (w.get_job() == 'N') return 1;
+        
+    }
+    return 0;
 }
 
+int company::Manager::set_job(int job)
+{   
+    std::srand(job+myId);
+    int jobCount = rand()%(myTeam.size()+1);
+    if (jobCount > freeTeamatesCouter) jobCount = freeTeamatesCouter;
+
+    std::cout << "\n"<< myName << " received " << job <<" job.\n";
+    std::cout << "give team_" << myId+1 << " " << jobCount << " jobs.\n";
+    for (Worker& w : myTeam)
+    {
+        if (freeTeamatesCouter <= 0) 
+        {
+            std::cout << "team_" << myId+1 << " have`t free workers\n";
+            return -1;
+        }
+
+        if (jobCount == 0) return 0;
+        
+        if (w.get_job() == 'N') // if worker has no job
+        {
+            w.set_job(65+rand()%3);
+            jobCount --;
+            freeTeamatesCouter --;
+            
+        }
+
+        if (freeTeamatesCouter <= 0) 
+        {
+            std::cout << "team_" << myId+1 << " have`t free workers\n";
+            return -1;
+        }
+        
+    }
+    return 1;
+};
+
+company::Principal::Principal(std::string name, std::vector<int> workersInTeamsAmount)
+{
+    myName = name;
+    for (int i = 0; i < workersInTeamsAmount.size(); i++)
+    {
+        std::string mName = "Tom_" + std::to_string(i+1);
+        std::vector<std::string> wNames;
+        for (int j = 0; j < workersInTeamsAmount[i]; j++)
+        {
+            wNames.push_back("Bob_" + std::to_string(j));
+        }
+        myTeam.push_back(Manager(mName,i,wNames));
+    }
+    freeTeamatesCouter = myTeam.size();
+};
+
+int company::Principal::set_job(int job)
+{ 
+   std::cout << myName << " give managers " << job << " job.\n\n";
+
+    for (Manager& m : myTeam)
+    {
+        if (m.has_free_worker() == 0) // teams has't free workers
+        {
+            freeTeamatesCouter --;
+        }
+        else
+        {
+            m.set_job(job);
+        }
+
+        if (freeTeamatesCouter == 0) 
+        {
+            std::cout << "\nAll workers has a job\n";
+            return -1;
+        }
+    }
+    std::cout << "-------------------------------------------------------------\n";
+    return 1;
+}
