6d7e6da1f948d92425d171e5e021d331bfee7a24
KonTN <konstantintukmachew@yandex.ru>
2022-11-03 22:48:15 +0300

    fixed 26.1

--- 26.1/src/main.cpp
+++ 26.1/src/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <tree.h>
+
+void print_tree(Tree &wood, int tree_num)
+{
+    Tree tree = wood.get_subtree()[tree_num];
+    std::cout << "root" << "->" << "\n";
+    for (Tree &big_branch : tree.get_subtree())
+    {
+        std::cout << "    " << big_branch.vilagerName;
+        if (!big_branch.get_subtree().empty()) std::cout << "->" << "\n";
+        else std::cout << "\n";
+
+        for (Tree &small_branch : big_branch.get_subtree())
+        {
+            std::cout << "        " << small_branch.vilagerName << "\n";
+        }
+    }
+}
+
 int main(int, char**) {
     // init
-    Tree myTree(nullptr);
-    srand(time(NULL));
+    Tree wood(nullptr);
+    srand(time(nullptr));
     
     // starting tree colonization 
     std::cout << "Please specify the names of the inhabited elves" << std::endl;
@@ -11,45 +29,57 @@ int main(int, char**) {
     for (int i = 0; i < 1; i++) // 5 trees
     {
         std::cout << i+1 << " tree:" << std::endl;
-        Tree* tree = myTree.add_branch();
+        Tree* tree = wood.add_branch();
         for (int j = 0; j < 3+rand()%3; j++) // big branches;
         {
             Tree* bigBranch = tree->add_branch();
-
-            std::cout << "\t";
+            std::cout << "please enter elf name: ";
             std::string elfName;
             std::cin >> elfName;
             bigBranch->vilagerName = elfName;
-
+            print_tree(wood,i);
             for (int k  = 0; k < 2+rand()%2; k++ ) // medium branches;
             {
                 Tree* medBranch = bigBranch->add_branch();
-
-                std::cout << "\t\t";
-                std::string elfName;
+                std::cout << "please enter elf name: ";
                 std::cin >> elfName;
                 medBranch->vilagerName = elfName;
+                print_tree(wood,i);
             }
         }
+        print_tree(wood,i);
     }
-
-    std::cout << "Please enter elf name to find his neghbors: ";
+    for (int i = 0 ; i < wood.get_subtree().size(); i++)
+    {
+        std::cout << i+1 << " Tree:\n";
+        print_tree(wood,i);
+    }
+    std::cout << "\nPlease enter elf name to find his neighbors: ";
     std::string name;
     std::cin >> name;
 
-    Tree* foundBranch = nullptr;
-    for (Tree t : myTree.get_subtree_recursive())
+    Tree* foundBranch;
+    for (Tree &t : wood.get_subtree_recursive())
     {
         if ((t.vilagerName == name)&&(name != "None"))
         {
-            foundBranch = t.get_root();
-            
-            std::cout << "found neghbors: ";
-            for (Tree t : foundBranch->get_subtree())
+            std::cout << "found neighbors: ";
+
+            if (t.get_subtree().empty())
             {
-                if ((t.vilagerName != "None")&&(t.vilagerName != name))
+                foundBranch = t.get_root();
+                std::cout << foundBranch->vilagerName << " ";
+            }
+            else
+            {
+                foundBranch = &t;
+            }
+
+            for (Tree &tr : foundBranch->get_subtree())
+            {
+                if ((tr.vilagerName != "None")&&(tr.vilagerName != name))
                 {
-                    std::cout << t.vilagerName << " ";
+                    std::cout << tr.vilagerName << " ";
                 }
             }
             std::cout << std::endl;
@@ -57,7 +87,7 @@ int main(int, char**) {
         } 
     }
 
-    std::cout << "Can't find any '" << name << "' elfs" << std::endl;
+    std::cout << "Can't find any '" << name << "' elf" << std::endl;
 }   
 
 
